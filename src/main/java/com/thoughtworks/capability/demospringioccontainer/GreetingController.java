198eb4375970b3664fa6b185968af1eb1c96ae5c
Ruoqian Cheng <ruoqian.cheng@RuoqiandeMacBook-Pro.local>
2020-11-09 16:39:23 +0800

    [evacheng]feat: completed p2s2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,28 @@
+
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
+
+
 @RestController
+@Scope(SCOPE_SINGLETON)
 public class GreetingController {
 
-    private final GreetingService greetingService;
+    private final ApplicationContext applicationContext;
 
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    public GreetingController(ApplicationContext applicationContext) {
+        System.out.println("Greetings.");
+        this.applicationContext = applicationContext;
     }
 
     @GetMapping("/greet")
     public String greet() {
-        return greetingService.sayHi();
+        return applicationContext.getBean(GreetingService.class).sayHi();
     }
 
 }
